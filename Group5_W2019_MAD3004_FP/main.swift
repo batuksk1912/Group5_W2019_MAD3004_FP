6850cbd25a89df56a8fbe8134fd9763f44d425de
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-26 16:21:53 -0500

    main methods need comment and re-order.

--- Group5_W2019_MAD3004_FP/main.swift
+++ Group5_W2019_MAD3004_FP/main.swift
@@ -8,22 +8,25 @@
 
 import Foundation
 
+
+//create 2 user object
 let user1 = User(userId: "1", password: "12345", loginStatus: "notloggedin")
 let user2 = User(userId: "2", password: "67890", loginStatus: "notloggedin")
 
-let admin1 = Administrator(userId: "3", password: "11111", loginStatus: "notloggedin", adminName: "gury", email: "gmail", canUpdateCatalog: true)
-let admin2 = Administrator(userId: "4", password: "22222", loginStatus: "notloggedin", adminName: "dani", email: "gmail", canUpdateCatalog: false)
+//create 2 administrator object
+let admin1 = Administrator(userId: "3", password: "11111", loginStatus: "notloggedin", adminName: "gury", email: "er@gmail.com", canUpdateCatalog: true)
+let admin2 = Administrator(userId: "4", password: "22222", loginStatus: "notloggedin", adminName: "dani", email: "dt@gmail.com", canUpdateCatalog: false)
 
 print("//Administrator Check//")
 
-print("For admin1 Object")
+print("Check if admin1 is a Admin!")
 if (admin1.canUpdateCatalog == true) {
     print("This user is a admin.")
 } else {
     print("Ordinary user.")
 }
 
-print("For admin2 Object")
+print("Check if admin2 is a Admin!")
 if (admin2.canUpdateCatalog == true) {
     print("This user is a admin.")
 } else {
@@ -31,27 +34,29 @@ if (admin2.canUpdateCatalog == true) {
 }
 print("********************")
 
+//create 2 customer object
 let customer1 = Customer(userId: "5", password: "33333", loginStatus: "notloggedin", customerName: "khushi", address: "toronto", email: "yahoo", creditCardInfo: "0000", shippingInfo: "toronto")
 let customer2 = Customer(userId: "6", password: "44444", loginStatus: "notloggedin", customerName: "batu", address: "north york", email: "hotmail", creditCardInfo: "0000", shippingInfo: "north york")
 
+//create manufacturer objects
 let manufacturer1 = Manufacturer(manufId: 1, manufName: "Apple")
 let manufacturer2 = Manufacturer(manufId: 1, manufName: "Samsung")
 
+//create product objects
 let product1 = Product(manufId: 1, manufName: "Apple", productId: 1, productName: "iPhone", price: 1200.00, quantity: 10)
 let product2 = Product(manufId: 2, manufName: "Samsung", productId: 2, productName: "Android", price: 1000.00, quantity: 6)
 
+//creating order1 and details and shopping cart's
 let shoppingcart1 = ShoppingCart(cartId: 1, productId: 1, quantity: 1, dateAdded: Date())
-let shoppingcart2 = ShoppingCart(cartId: 2, productId: 2, quantity: 2, dateAdded: Date())
-
 let order1 = Order(orderId: 1, dateCreated: Date(), dateShipped: Date(), CustomerName: "dani", CustomerId: 5, status: "delivered", shippingId: 1)
-let order2 = Order(orderId: 2, dateCreated: Date(), dateShipped: Date(), CustomerName: "batu", CustomerId: 6, status: "delivered", shippingId: 2)
-
 let orderdetail1 = OrderDetails(orderId: 1, productId: 1, productName: "iPhone", quantity: 1, unitCost: 1200.00)
-let orderdetail2 = OrderDetails(orderId: 2, productId: 2, productName: "Android", quantity: 2, unitCost: 2000.00)
-
 let shippinginfo1 = ShippingInfo(shippingId: 1, shippingType: "express", shippingCost: 10, shippingRegionId: 1)
-let shippinginfo2 = ShippingInfo(shippingId: 2, shippingType: "regular", shippingCost: 5, shippingRegionId: 2)
 
+//creating order2 and details and shopping cart's
+let shoppingcart2 = ShoppingCart(cartId: 2, productId: 2, quantity: 2, dateAdded: Date())
+let order2 = Order(orderId: 2, dateCreated: Date(), dateShipped: Date(), CustomerName: "batu", CustomerId: 6, status: "delivered", shippingId: 2)
+let orderdetail2 = OrderDetails(orderId: 2, productId: 2, productName: "Android", quantity: 2, unitCost: 2000.00)
+let shippinginfo2 = ShippingInfo(shippingId: 2, shippingType: "regular", shippingCost: 5, shippingRegionId: 2)
 
 print("---Test Login and Register---")
 
@@ -100,8 +105,8 @@ print("Set Shopping Carts to User")
 customer1.setShoppingCart(shoppingCart: shoppingcart1)
 customer2.setShoppingCart(shoppingCart: shoppingcart2)
 
-order1.placeOrder(shippingInfo: shippinginfo1, orderDetails: orderdetail1, status: "Delivered")
-order2.placeOrder(shippingInfo: shippinginfo2, orderDetails: orderdetail2, status: "Delivered")
+order1.placeOrder(shippingInfo: shippinginfo1, orderDetails: orderdetail1, status: "Waiting Delivery")
+order2.placeOrder(shippingInfo: shippinginfo2, orderDetails: orderdetail2, status: "Waiting Delivery")
 
 customer1.setOrders(orders: order1)
 customer2.setOrders(orders: order2)
